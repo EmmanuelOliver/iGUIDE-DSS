6dccb430f3de7893c889bbb62ded230ddad58536
EmmanuelOliver <emmanuel.felipe.oliver@gmail.com>
2023-05-18 02:12:15 +0800

    2nd Commit

--- server/routes/counseling_session.js
+++ server/routes/counseling_session.js
@@ -4,8 +4,15 @@ const {
     getCounselingSession,
     createCounselingSession,
     deleteCounselingSession,
-    updateCounselingSession
+    updateCounselingSession,
+    addResultsToSession,
+    addActionToSession,
+    addActivityToSession,
+    addNoteToSession,
+    getSessionStatus,
+    updateSessionStatus
 } = require('../controllers/counselingController')
+
 const requireAuth = require('../middleware/requireAuthentication')
 
 const router = express.Router()
@@ -21,5 +28,13 @@ router.post('/', createCounselingSession)
 router.delete('/:id', deleteCounselingSession)
 
 router.patch('/:id', updateCounselingSession)
+
+// New routes for updating the session with results, actions, activities, notes and status
+router.post('/:id/results', addResultsToSession)
+router.post('/:id/actions', addActionToSession)
+router.post('/:id/activities', addActivityToSession)
+router.post('/:id/notes', addNoteToSession)
+router.get('/:id/status', getSessionStatus)
+router.patch('/:id/status', updateSessionStatus)
 
 module.exports = router
